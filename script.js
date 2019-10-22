b304a3a5687837442da24f97f5895d749b178712
“Erin <erinkelley27@gmail.com>
2019-10-22 15:33:14 -0400

    add blanks for each letter in a word

--- script.js
+++ script.js
@@ -8,10 +8,16 @@ let words = [
 
 let getRandom = document.querySelector('.get-random')
 console.log(getRandom)
+let lettersList = document.querySelector('.letters-list')
 
 getRandom.addEventListener('click', function (evt) {
     evt.preventDefault()
     console.log('button works')
     let randomWord = words[Math.floor(Math.random() * words.length)]
     console.log(randomWord)
+    for (let i = 0; i < randomWord.length; i++) {
+        let letterBlank = document.createElement('li')
+        letterBlank.innerHTML = '_'
+        lettersList.appendChild(letterBlank)
+    }
 })
